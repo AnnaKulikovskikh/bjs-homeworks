b35f69c3d1f4425e8ec3c0a9fb21e3714ef906dc
Anna Kulikovskikh <k_anna_i@mail.ru>
2020-05-06 23:33:16 +0300

    Task 1.3 was corrected

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,24 +1,28 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
     "use strict";
-    let bodyCredit = parseInt(amount) - parseInt(contribution);
-    let today = new Date();
-    let year = date.getFullYear() - today.getFullYear();
-    let month = date.getMonth() - today.getMonth();
+    if (isNaN(+percent)) {
+        return(`Параметр \"Процентная ставка\" содержит неправильное знаение \"${percent}\"`);
+    };
+    if (isNaN(+contribution)) {
+        return(`Параметр \"Первоначальный взнос\" содержит неправильное знаение \"${contribution}\"`);
+    };
+    if (isNaN(+amount)) {
+        return(`Параметр \"Сумма кредита\" содержит неправильное знаение \"${amount}\"`);
+    };
+    const bodyCredit = parseInt(amount) - parseInt(contribution);
+    const today = new Date();
+    const year = date.getFullYear() - today.getFullYear();
+    const month = date.getMonth() - today.getMonth();
     if (date.getDate() < date.today) month = month-1;
     month += year*12; // month - число месяцев
-    let monthPercent = parseInt(percent)/12/100;
-    let pay = bodyCredit * (monthPercent + monthPercent/(Math.pow((1 + monthPercent),month) - 1));
-    let totalAmount = Number((pay * month).toFixed(2));
+    const monthPercent = parseInt(percent)/12/100;
+    const pay = bodyCredit * (monthPercent + monthPercent/(Math.pow((1 + monthPercent),month) - 1));
+    const totalAmount = Number((pay * month).toFixed(2));
     // код для задачи №1 писать здесь
     return totalAmount;
 }
 
 function getGreeting(name) {
-    if (name == undefined || name == "") {
-        name = "Аноним";
-    }
-    let greeting = `Привет мир! Меня зовут ${name}.`;
-    console.log(greeting); 
     // код для задачи №2 писать здесь
-    return greeting;
+    return  (`Привет мир! Меня зовут ${name || "Аноним"}`);
 }
