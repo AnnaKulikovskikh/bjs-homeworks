64d6eedcaa23f22bdc5d5f077a6f07b0f3a8646f
Anna Kulikovskikh <k_anna_i@mail.ru>
2020-05-11 23:46:05 +0300

    Task 2.2 complete

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,30 @@
 function getAnimalSound(animal) {
+    if (isNaN(animal)) {
+        return null;
+    } else {
+        let animalSound = animal.sound;
+        return animalSound;
+    }    
     // код для задачи №1 писать здесь
     // return animalSound;
 }
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
-    // return averageMark
+    let average = 0;
+    for (let i of marks){
+        average += i;
+    }
+    average = Math.round(average/marks.length);
+    if (isNaN(average)) {
+        average = 0;
+    }    
+    return average;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    // код для задачи №3 писать 
+    //now = date.setMilliseconds(ms);
+
+    return verdict;
 }
