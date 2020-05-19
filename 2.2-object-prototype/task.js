27353858864b2095d0b17e21a464ec42587c5bf1
Anna Kulikovskikh <k_anna_i@mail.ru>
2020-05-19 23:39:32 +0300

    Task 2.2 test.js changed

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -11,13 +11,13 @@ function getAnimalSound(animal) {
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
+    if (marks.length === 0) {
+        return 0;
     let summary = 0;
     for (let i of marks){
         summary += parseFloat(i);
     }
     const average = Math.round(summary/marks.length);
-    if (marks.length == 0) {
-        average = 0;
     }    
     return average;
 }
@@ -26,5 +26,5 @@ function getAverageMark(marks) {
 function checkBirthday(birthday) {
     // код для задачи №3 писать 
     //now = date.setMilliseconds(ms);
-    return (Date.now() - birthday)/31557600000 >=18;
+    return (Date.now() - (+new Date(birthday))/31557600000 >=18;
 }
