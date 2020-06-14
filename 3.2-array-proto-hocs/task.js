9577a2d3ee84c6cc35c921b1ea73ba88653979a1
AnnaKulikovskikh <59337789+AnnaKulikovskikh@users.noreply.github.com>
2020-06-14 13:19:46 +0200

    update task 3.2 140620

--- 3.2-array-proto-hocs/task.js
+++ 3.2-array-proto-hocs/task.js
@@ -1,9 +1,3 @@
-class userError extends Error {
-  constructor(message){
-    super(message);
-    this.name = "userError";
-  }
-}
 function sum(...args) {
   sleep(300); 
  // console.log(`args ${args}`);
@@ -15,36 +9,39 @@ function sum(...args) {
 function memorize(fn, limit) {
   let count = 0;
   const memory = [];
-  if (limit === undefined || limit ==0){
-     throw new userError("Не задано число сохраняемых значений");;
+  if (limit === undefined || limit == 0){
+     throw new Error("Не задано число сохраняемых значений");;
      };
   return  function innerFunc(...args){
-    //alert(`count=${count}`);
-    //alert(`длина массива=${memory.length}`);
+    //console.log(`count=${count}`);
+    //console.log(`длина массива=${memory.length}`);
 
     let exist = memory.find(item => compareArrays(item.args,args));
-    if (exist != undefined){return exist.result};
+    if (exist){
+      return exist.result};
 
     if (memory.length == limit){
       memory[count] = ({args: args, result: fn(...args)});
       count ++;
       if (count == limit){count = 0};
+      return memory[count-1].result;
     } else {
       memory.push({args: args, result: fn(...args)});
+      return memory[memory.length-1].result
     }
-    alert(memory);
-    return fn(...args);
+    //console.log(memory);
+    //return fn(...args);
   };
 }
-const mSum = memorize(sum,4);
-alert(mSum(1,2,3));
-alert(mSum(4,5,6));
-alert(mSum(7,8));
-alert(mSum(1,2,3));
-alert(mSum(10,11,12));
-alert(mSum(13,14,15));
+const mSum = memorize(sum,3);
+console.log(mSum(1,2,3));
+console.log(mSum(4,5,6));
+console.log(mSum(7,8));
+console.log(mSum(1,2,3));
+console.log(mSum(10,11,12));
+console.log(mSum(13,14,15));
 const mSum1 = memorize(sum,3);
-alert(mSum1(6,4,9));
+console.log(mSum1(6,4,9));
 
 function sleep(milliseconds) 
 {
@@ -55,3 +52,23 @@ function sleep(milliseconds)
 function compareArrays(arr1,arr2){
   return arr1.length == arr2.length && arr1.every((v,i)=>v === arr2[i]);
 }
+//console.log(compareArrays([8, 9], [6])); // false, разные значения
+//console.log(compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5])); // false, разные значения
+//console.log(compareArrays([9, 2, 4, 8, 2], [9, 2, 4])); // false, разные значения
+//console.log(compareArrays([1, 2, 3], [2, 3, 1])); // false, разные индексы, хотя и одинаковые значения
+//console.log(compareArrays([8, 1, 2], [8, 1, 2])); // true
+console.log("--------------------");
+function testCase(testFunction,timer){
+  arra = [ [1,2,3], [1,2], [1,2,3], [1,2], [9,5,2,4],[15,26,178,4],[2],[67,345,768,45,34,2] ];
+  let  i = 0;
+  const summa = memorize(sum,100);
+  while (i != 15){
+    arra.forEach(function(item, i, arr){summa(item);});
+    i++
+    };
+}    
+console.time('timer1');
+testCase(sum,'timer1');
+console.timeEnd('timer1');
+//sum timer1: 2409.071ms
+//mSum timer1: 2411.423ms
