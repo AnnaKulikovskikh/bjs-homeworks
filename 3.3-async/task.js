f0e4a8cde58556bc9ec9e9a11b03157aed9c37d5
AnnaKulikovskikh <59337789+AnnaKulikovskikh@users.noreply.github.com>
2020-06-17 22:48:29 +0200

    Update task.js

--- 3.3-async/task.js
+++ 3.3-async/task.js
@@ -1,61 +1,48 @@
-  function getCurrentFormattedTime(){
-    let date = new Date();
-    return String(date.getHours()) + ':' + String(date.getMinutes());
-  }
-
 class AlarmClock{
-  constructor(timerId){
-    this.alarmCollection = [];
-    this.timerId = timerId;
-  }
-
-  addClock(time,callback,id){
-    if (id==undefined){throw new Error('Невозможно идентифицировать будильник. Параметр id не передан.')};
-    if (this.alarmCollection.find(item => item.id==id)) {console.error('id repeat');
-    } else {
-      this.alarmCollection.push({time: time, callback: callback(), id: id});
-    }  
-  }
-
-  removeClock(id){
-    if (this.alarmCollection.find(item => item.id==id)){
-      this.alarmCollection.splice(this.alarmCollection.findIndex(item => item.id==id),1);
+    constructor(timerId){
+      this.alarmCollection = [];
+      this.timerId = {};
     }
-  }
-
-  start(){
-    //function checkClock(){ 
-      if (this.alarmCollection.forEach(element=>element.time)==getCurrentFormattedTime()){
-        console.log(element.callback);
+  
+    getCurrentFormattedTime(){
+      let date = new Date();
+      return String(date.getHours()) + ':' + String(date.getMinutes());
+    }
+  
+    start(){
+  
+      let checkClock = (alarm) => 
+      {if (alarm.time == this.getCurrentFormattedTime()){
+          alarm.callback();
+        }
       }
-
-      //if (getCurrentFormattedTime == )
-
-    //}
-
-  }
-
-//Создайте функцию проверки (checkClock), которая принимает звонок и проверяет: если текущее время совпадает со временем звонка, то вызывайте колбек.
-
-  stop(){
-
-  }
-
-  printAlarms(){
-    console.log(`Печать всех будильников в количестве: ${this.alarmCollection.length}`);
-    this.alarmCollection.forEach(element=>console.log(`Будильник №${element.id} заведен на ${element.time}`));
-  }
-
-  clearAlarms(){
-  }
-}
-
-
-let phoneAlarm = new AlarmClock();
-phoneAlarm.addClock('20:23',()=>console.log('Пора вставать'),1);
-phoneAlarm.addClock('09:01',()=>{console.log('Давай, вставай уже!'); phoneAlarm.removeClock(2)},2);
-phoneAlarm.addClock('20:32',()=>console.log('Иди учиться'),3);
-//phoneAlarm.removeClock(1);
-//phoneAlarm.getCurrentFormattedTime();
-phoneAlarm.start();
-//phoneAlarm.printAlarms();
+      this.timerId = setInterval(() => this.alarmColletion.forEach(element => checkClock(element)),1000);  
+    }
+  
+    stop(){
+      if (this.timerId != {}){clearInterval(this.timerId)};
+    }
+  
+    addClock(time,callback,id){
+      if (id==undefined){throw new Error('Невозможно идентифицировать будильник. Параметр id не передан.')};
+      if (this.alarmCollection.find(item => item.id==id)) {console.error('id repeat');
+      } else {
+        this.alarmCollection.push({time: time, callback: callback, id: id});
+      }  
+    }
+  
+    removeClock(id){
+      if (this.alarmCollection.find(item => item.id==id)){
+        this.alarmCollection.splice(this.alarmCollection.findIndex(item => item.id==id),1);
+      }
+    }
+  
+    printAlarms(){
+      console.log(`Печать всех будильников в количестве: ${this.alarmCollection.length}`);
+      this.alarmCollection.forEach(element=>console.log(`Будильник №${element.id} заведен на ${element.time}`));
+    }
+  
+    clearAlarms(){
+      this.stop();
+      this.alarmCollection = [];
+      //this.alarmCollection.forEach(item => this.removeClock(item.id));
